6209380199d839267bbd6a9fc771be03f273be37
kambekson <k.28.06.2002@gmail.com>
2024-03-23 20:22:30 +0500

    add posts url slug

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post
 
-def post_list(request):
+def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render (request,'posts/posts_list.html', {'posts': posts}) 
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render (request,'posts/post_page.html', {'post': post}) 
