1118b34f466d03494904bc78ddff1b757674220b
kambekson <k.28.06.2002@gmail.com>
2024-04-09 16:25:37 +0500

    create signup form

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def register_view(request):
-    return render (request,'users/register.html') 
-
-def post_page(request, slug):
-    post = post.objects.get(slug=slug)
-    return render (request,'posts/post_page.html', {'post': post}) 
+    if request.method == 'POST':
+        form = UserCreationForm (request.POST)
+        if form.is_valid ():
+            form.save ()
+            return redirect ('posts:list')
+    else:
+        form = UserCreationForm ()
+    return render (request,'users/register.html', {'form': form}) 
