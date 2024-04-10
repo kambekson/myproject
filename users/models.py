281fd75d0bd1626fa346153d714ad8f412fcf4fc
kambekson <k.28.06.2002@gmail.com>
2024-04-11 02:05:34 +0500

    create user profile

--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from posts.models import Post
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=30, unique=True)
     date_of_birth = models.DateField(null=True, blank=False)  # Изменено на blank=False
-    profile_picture = models.ImageField(default='fallback.png', blank=True)
+    profile_picture = models.ImageField(default='default.png', blank=True)
     first_name = models.CharField(max_length=30, blank=False)  # Изменено на blank=False
     last_name = models.CharField(max_length=30, blank=False)  # Изменено на blank=False
 
@@ -14,3 +15,12 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=200, blank=True)
+    status = models.CharField(max_length=60, blank=True)
+    posts = models.ManyToManyField(Post, related_name='user_posts')  # New field for user's posts
+
+    def __str__(self):
+        return self.user.username
--- users/views.py
+++ users/views.py
@@ -1,8 +1,25 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.urls import reverse_lazy
+from .models import UserProfile
+from posts.models import Post
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('posts:list')
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
 
 def register_view(request):
     if request.method == 'POST':
@@ -19,16 +36,11 @@ def logout_view(request):
     logout(request)
     return redirect('posts:list')
 
-def login_view(request):
-    if request.method == 'POST':
-        form = CustomAuthenticationForm(request, request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('posts:list')
-    else:
-        form = CustomAuthenticationForm()
-    return render(request, 'users/login.html', {'form': form})
+@login_required
+def profile(request, username):
+    try:
+        user_profile = UserProfile.objects.get(user__username=username)
+        user_posts = user_profile.posts.filter(author=user_profile.user)  # Фильтруем по автору, используя объект пользователя
+        return render(request, 'users/profile.html', {'user_profile': user_profile, 'user_posts': user_posts})
+    except UserProfile.DoesNotExist:
+        return render(request, 'users/profile.html', {'error_message': 'User profile not found'})
